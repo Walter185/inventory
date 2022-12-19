e2e96ba4c222ca48a3c5ddfa90009175931f887f
Walter Liendo <walter.liendo@gmail.com>
2022-12-18 22:12:00 -0300

    Remove a hardcoded Django secret key

--- inventory/apps.py
+++ inventory/apps.py
@@ -1,5 +1,6 @@
 from django.apps import AppConfig
-
+from django.core.management.utils import get_random_secret_key
+print(get_random_secret_key())
 
 class InventoryConfig(AppConfig):
     name = 'inventory'
